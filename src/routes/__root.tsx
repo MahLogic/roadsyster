439799800d1f546a2fea1234a2527714e762aebe
Kelly Mahlangu <220073684+mahkellz@users.noreply.github.com>
2026-01-12 20:37:50 +0200

    fix:fixed vercel error

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,7 +2,7 @@ import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { Analytics } from '@vercel/analytics/next'
+import { Analytics } from '@vercel/analytics/react'
 import appCss from '../styles.css?url'
 import { Toaster } from '@/components/ui/sonner'
 
