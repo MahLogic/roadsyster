e8ea39578ba3fe34c68d2d0efbeb327679a19c62
Kelly Mahlangu <220073684+mahkellz@users.noreply.github.com>
2026-01-12 20:34:25 +0200

    add vercel analytics component

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,7 +2,7 @@ import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
+import { Analytics } from '@vercel/analytics/next'
 import appCss from '../styles.css?url'
 import { Toaster } from '@/components/ui/sonner'
 
@@ -49,6 +49,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         <MainLayout>{children}</MainLayout>
+        <Analytics />
         <TanStackDevtools
           config={{
             position: 'bottom-right',
